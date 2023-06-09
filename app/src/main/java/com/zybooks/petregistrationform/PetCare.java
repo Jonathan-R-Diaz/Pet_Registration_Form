ee53a51ce05ffe52f9698884efb54600c88f0ad9
Jonathan Diaz <101024433+BACPoox@users.noreply.github.com>
2023-06-09 02:01:21 -0400

    Added reset and submit button functionality

--- app/src/main/java/com/zybooks/petregistrationform/PetCare.java
+++ app/src/main/java/com/zybooks/petregistrationform/PetCare.java
@@ -1,4 +1,35 @@
+//MODEL
 package com.zybooks.petregistrationform;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PetCare {
+    public PetCare(){}
+
+    public boolean regex(String input, String re){
+        Pattern pt = Pattern.compile(re);
+        Matcher mt = pt.matcher(input);
+        return mt.matches();
+    }
+
+    public boolean checkMicroChip(String microchip){
+        System.out.println("Microchip: " + microchip);
+        return regex(microchip,"^[A-Z0-9]{5,15}$");
+    }
+
+    public boolean checkName(String name){
+        System.out.println("Name: " + name);
+        return regex(name, "\\b[A-Z][a-zA-Z]*\\b(?:\\s+[A-Z][a-zA-Z]*\\b)*");
+    }
+
+    public boolean checkEmail(String email){
+        System.out.println("Email: " + email);
+        return regex(email, "^[A-Za-z0-9._%+-]{3,}@[A-Za-z0-9.-]+\\.(?:edu|co|com|gal)$");
+    }
+
+    public boolean checkCodes(int code1, int code2){
+        System.out.println("Checking codes... ");
+        return code1 == code2;
+    }
 }
