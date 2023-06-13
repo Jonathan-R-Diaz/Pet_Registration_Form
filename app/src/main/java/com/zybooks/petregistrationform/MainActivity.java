7a20c6bec88a823ca92e3920608e5f30a0878120
Jonathan Diaz <101024433+BACPoox@users.noreply.github.com>
2023-06-13 02:17:59 -0400

    Removed test email string

--- app/src/main/java/com/zybooks/petregistrationform/MainActivity.java
+++ app/src/main/java/com/zybooks/petregistrationform/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.relative_main);
+        setContentView(R.layout.linear_main);
 
         mPetCare = new PetCare();
 
