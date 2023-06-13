69c2b59f7ecb1117a3dd68c5b04dcb6e65048d8f
Jonathan Diaz <101024433+BACPoox@users.noreply.github.com>
2023-06-13 02:14:50 -0400

    Added inputType password

--- app/src/main/java/com/zybooks/petregistrationform/MainActivity.java
+++ app/src/main/java/com/zybooks/petregistrationform/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.constraint_main);
+        setContentView(R.layout.relative_main);
 
         mPetCare = new PetCare();
 
