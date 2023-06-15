bfdc59649f39d88bbe2954e4cf0688004e9f5f38
Jonathan Diaz <jrd17j@fsu.edu>
2023-06-15 18:29:20 -0400

    Added email hint to constraint layout

--- app/src/main/java/com/zybooks/petregistrationform/MainActivity.java
+++ app/src/main/java/com/zybooks/petregistrationform/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.linear_main);
+        setContentView(R.layout.constraint_main);
 
         mPetCare = new PetCare();
 
